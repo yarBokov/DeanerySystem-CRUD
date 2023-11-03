5ea2b37d75886e1e5f81a1fbb61836df8c345d01
yarBokov <bokovjaroslav@gmail.com>
2023-11-03 23:12:14 +0300

    Subjectsview similar to groupsview completed. Fixed dialog window color conflicts.

--- DeanerySystem/Services/ISubjectService.cs
+++ DeanerySystem/Services/ISubjectService.cs
@@ -1,9 +1,14 @@
 ﻿using DeanerySystem.Data.Entities;
+using DeanerySystem.Models;
 
 namespace DeanerySystem.Services
 {
     public interface ISubjectService
     {
+        bool CheckIfNonEditable(Subject subject);
+        Task<MethodResult> DeleteSubjectAsync(int subjectId);
+        Task<Subject> GetSubjectById(int subjectId);
         Task<IEnumerable<Subject>> GetSubjectsAsync();
+        Task<MethodResult> SaveSubjectAsync(Subject subject);
     }
 }
